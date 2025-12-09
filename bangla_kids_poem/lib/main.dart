69a2166a40c0a6fa4ac442dee5b906249ef4bdb6
Manan Ahmed Broti <54348038+AhmedManan@users.noreply.github.com>
2025-12-09 22:50:22 +0600

    Update main.dart

--- bangla_kids_poem/lib/main.dart
+++ bangla_kids_poem/lib/main.dart
@@ -15,12 +15,12 @@ class MyApp extends StatelessWidget{
       theme: ThemeData(primarySwatch: Colors.purple),
       darkTheme: ThemeData(primarySwatch: Colors.deepPurple),
       debugShowCheckedModeBanner: false,
-      home:homeActivity());
+      home:HomeActivity());
   }
 }
 
-class homeActivity extends StatelessWidget{
-    const homeActivity({super.key});
+class HomeActivity extends StatelessWidget{
+    const HomeActivity({super.key});
 
     mySnackBar(message, context)
     {
@@ -40,13 +40,19 @@ class homeActivity extends StatelessWidget{
         actions: [
           IconButton(onPressed: (){mySnackBar("message", context);}, icon: Icon(Icons.info))
         ],
-
       ),
-      body: Text('Manan'),
       drawer: Drawer(
         child: ListView(
           children: [
-            DrawerHeader(child: Text('Manan')),
+            DrawerHeader(
+              padding: EdgeInsetsGeometry.all(0),
+              child: UserAccountsDrawerHeader(
+                decoration: BoxDecoration(color: Colors.amber),
+                accountName: Text("Manan",  style: TextStyle(color: Colors.blue),),
+                accountEmail: Text("Email", style: TextStyle(color: Colors.blue),),
+                currentAccountPicture: Image.network("https://scontent-sin2-2.xx.fbcdn.net/v/t39.30808-6/453014603_122141494382264015_394986228119248179_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=8qCNOvza7zgQ7kNvwGY0BNC&_nc_oc=AdkzVfxHK73Tim7W7sU5moJ9UGMnxa1VjqD-0Rs_Z21F_qMTzrwJE1aVei4Wj2ktiLcbr2MFPGMmCdphUWfzmCtA&_nc_zt=23&_nc_ht=scontent-sin2-2.xx&_nc_gid=fT3QsUPdPNwn-PFJoyAdZA&oh=00_AfmtuqvaeQ2u6qcqLqxIf1mXqAaz7_rv8zBg-K1KWmOVoA&oe=693B3685"),
+              ),
+            ),
             ListTile(leading: Icon(Icons.account_box_outlined), title: Text('data'),),
             ListTile(leading: Icon(Icons.account_box_outlined),title: Text('bangla'),)
           ],
@@ -64,7 +70,24 @@ class homeActivity extends StatelessWidget{
         onPressed: (){},
         elevation: 5,
         child: Icon(Icons.info)
-      )
+      ),
+      body: 
+      // Container(
+      //   height: 200,
+      //   width: 250,
+      //   alignment: Alignment.center,
+      //   decoration: BoxDecoration(
+      //     color: Colors.blue
+      //   ),
+      //   child: Text("Manan"),
+      // ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          TextButton(onPressed: (){}, child: Text('data')),
+          ElevatedButton(onPressed: (){}, child: Text("Button"))
+      ],),
+
     );
   }
 }
