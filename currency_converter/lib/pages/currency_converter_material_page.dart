e0183c9a8c7cad3b83f38948b169454d6520a96c
AhmedManan <ahmed.mnn@outlook.com>
2025-12-28 01:43:44 +0600

    Updated Page Design
    * Added convert button
    * Added appBar
    * Minor update

--- currency_converter/lib/pages/currency_converter_material_page.dart
+++ currency_converter/lib/pages/currency_converter_material_page.dart
@@ -27,27 +27,59 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text(
+          "Currency Converter",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrange,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              "Currency Converter",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Enter amount in BDT',
-                hintStyle: TextStyle(color: Colors.black),
-                prefixIcon: Icon(Icons.monetization_on_outlined),
-                prefixIconColor: Colors.black,
-                filled: true,
-                fillColor: Colors.white,
-                focusedBorder: activeBorder,
-                enabledBorder: inactiveBorder,
+              "0",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
               ),
-              keyboardType: const TextInputType.numberWithOptions(
-                decimal: true,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter amount in BDT',
+                  hintStyle: TextStyle(color: Colors.black),
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: Colors.black,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: activeBorder,
+                  enabledBorder: inactiveBorder,
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  debugPrint('Convert button pressed');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: (Colors.deepOrange),
+                  foregroundColor: (Colors.white),
+                  minimumSize: Size(double.infinity, 50),
+                  shape: ContinuousRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                child: Text("Convert"),
               ),
             ),
           ],
